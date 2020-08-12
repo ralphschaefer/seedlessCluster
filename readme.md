5c4697c7c438c911bc415bed912f5ce3983f0a8c
Ralph Schäfer <raschaef@gmx.de>
2020-08-13 00:13:01 +0200

    add docu

---
+++ readme.md
@@ -0,0 +1,10 @@
+cluster demo:
+-------------
+
+* custom service discovery
+* akka management
+* cluster routing
+* akka distributed data
+  - [scaladays talk distributed data](https://www.youtube.com/watch?v=NQKxDxn5olM)
+  - [CRDT (heise)](https://m.heise.de/developer/artikel/Verteilte-Daten-ohne-Muehe-Conflict-Free-Replicated-Data-Types-3944421.html?seite=all)
+* custom CRDT
--- src/main/scala/my/clusterDemo/actors/Sum.scala
+++ src/main/scala/my/clusterDemo/actors/Sum.scala
@@ -8,7 +8,8 @@ import my.clusterDemo.dtos.dData
 
 import scala.concurrent.duration.DurationInt
 
-// https://www.youtube.com/watch?v=NQKxDxn5olM
+// [scaladays talk distributed data](https://www.youtube.com/watch?v=NQKxDxn5olM)
+// [CRDT(heise)](https://m.heise.de/developer/artikel/Verteilte-Daten-ohne-Muehe-Conflict-Free-Replicated-Data-Types-3944421.html?seite=all)
 
 class Sum(startValue: Int) extends Actor with ActorLogging {
 
